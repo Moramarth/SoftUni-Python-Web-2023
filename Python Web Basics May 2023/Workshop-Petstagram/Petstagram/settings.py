aa66292ec1302fd0dbcaa8a85887fd16535df0a2
George Lazarov <122516587+Moramarth@users.noreply.github.com>
2023-06-03 12:53:00 +0300

    Created required apps
    Added apps, premade templates and static files

--- Python Web Basics May 2023/Workshop-Petstagram/Petstagram/settings.py
+++ Python Web Basics May 2023/Workshop-Petstagram/Petstagram/settings.py
@@ -39,6 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # our apps
+    'Petstagram.photos',
+    'Petstagram.pets',
+    'Petstagram.accounts',
+    'Petstagram.common',
 ]
 
 MIDDLEWARE = [
@@ -123,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
