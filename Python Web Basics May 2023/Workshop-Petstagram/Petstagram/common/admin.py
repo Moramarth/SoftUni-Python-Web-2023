244cf7e883d0e743a207b7565b6b9d4868dfdaa5
George Lazarov <122516587+Moramarth@users.noreply.github.com>
2023-06-12 12:06:10 +0300

    Added models and migrations

--- Python Web Basics May 2023/Workshop-Petstagram/Petstagram/common/admin.py
+++ Python Web Basics May 2023/Workshop-Petstagram/Petstagram/common/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from Petstagram.common.models import Comment, Like
+
+
 # Register your models here.
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["text", "date_time_of_publication", "to_photo"]
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ["to_photo"]
